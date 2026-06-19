e8d4453fad2563dec933d62a24465cb4e2d4e0db
poe-jo <anna870525@naver.com>
2026-06-19 12:40:27 +0900

    chore: configure dev server to auto-open in Google Chrome

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Force Vite to open the page in Google Chrome
+process.env.BROWSER = 'chrome'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Enjoy_Cooking/'
+  base: '/Enjoy_Cooking/',
+  server: {
+    open: true
+  }
 })
